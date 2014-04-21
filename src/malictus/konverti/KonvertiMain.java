05924cacf815681022e510a4721a0c9efb960926
Jim Halliday <malictus@gmail.com>
2014-04-21 01:08:24 -0400

    added imagemagick

--- src/malictus/konverti/KonvertiMain.java
+++ src/malictus/konverti/KonvertiMain.java
@@ -13,6 +13,11 @@ public class KonvertiMain {
 	//sox executable - windows only for now
 	public static final String SOX_PATH = "sox/win/sox.exe";
 	public static File SOX_FILE;
+	//imagemagick - 64-bit windows for now
+	private static final String IM_CONVERT_PATH = "ffmpeg/win_64/bin/ffmpeg.exe";
+	private static final String IM_IDENTIFY_PATH = "ffmpeg/win_64/bin/ffprobe.exe";
+	public static File IM_CONVERT_FILE;
+	public static File IM_IDENTIFY_FILE;
 
 	public static void main(String[] args) {
 		//set look and feel
@@ -37,6 +42,17 @@ public class KonvertiMain {
 		SOX_FILE = new File(SOX_PATH);
 		if (!SOX_FILE.exists() || !SOX_FILE.canRead() || !SOX_FILE.canExecute()) {
 			JOptionPane.showMessageDialog(null, "Fatal error. Sox executable is missing", "Sox missing", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+		//init imagemagick
+		IM_CONVERT_FILE = new File(IM_CONVERT_PATH);
+		if (!IM_CONVERT_FILE.exists() || !IM_CONVERT_FILE.canRead() || !IM_CONVERT_FILE.canExecute()) {
+			JOptionPane.showMessageDialog(null, "Fatal error. ImageMagick convert executable is missing", "IM convert missing", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+		IM_IDENTIFY_FILE = new File(IM_IDENTIFY_PATH);
+		if (!IM_IDENTIFY_FILE.exists() || !IM_IDENTIFY_FILE.canRead() || !IM_IDENTIFY_FILE.canExecute()) {
+			JOptionPane.showMessageDialog(null, "Fatal error. ImageMagick identify executable is missing", "IM identity missing", JOptionPane.ERROR_MESSAGE);
 			System.exit(1);
 		}
 		
